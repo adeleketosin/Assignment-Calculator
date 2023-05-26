257896323bef3b1cf870d2b02a1e5a58b3f48027
Adeleke Oluwatosin <125203704+adeleketosin@users.noreply.github.com>
2023-05-26 11:19:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const buttons = document.getElementsByClassName('tosin')
 const displayTray= document.getElementById('display');
+var equaltoClicked = false;
+
+let oper=['*','+','/','.','-']
 
 for(let button of buttons){
     button.addEventListener('click', () => {
@@ -8,44 +11,77 @@ for(let button of buttons){
                  displayTray.value ="";
                 break;
                 case ")":
-                 displayTray.value += button.innerText;
+                    if (equaltoClicked ==false)displayTray.value += button.innerText
+                    else reset(button.innerText); 
                 break;
             case "1":
-                displayTray.value += button.innerText;
+                if (equaltoClicked ==false)displayTray.value += button.innerText
+               else reset(button.innerText); 
                 break;
             case "2":
-                displayTray.value +=button.innerText;
+               if(equaltoClicked ==false)displayTray.value += button.innerText
+               else reset(button.innerText); 
+            
                 break; 
             case "3":
-                displayTray.value +=button.innerText;
+                // displayTray.value +=button.innerText;
+                if (equaltoClicked ==false)displayTray.value += button.innerText
+               else reset(button.innerText); 
+
                 break; 
             case "(":
-                displayTray.value +=button.innerText;
+                // displayTray.value +=button.innerText;
+                if (equaltoClicked ==false)displayTray.value += button.innerText
+               else reset(button.innerText); 
+
                 break; 
             case "4":
-                displayTray.value +=button.innerText;
+                // displayTray.value +=button.innerText;
+                if (equaltoClicked ==false)displayTray.value += button.innerText
+                else reset(button.innerText); 
+
                 break; 
             case "5":
-                displayTray.value +=button.innerText;
+                if (equaltoClicked ==false)displayTray.value += button.innerText
+                else reset(button.innerText); 
+
                 break; 
             case "6":
-                displayTray.value +=button.innerText;
+                if (equaltoClicked ==false)displayTray.value += button.innerText
+                else reset(button.innerText); 
+
                 break;
               case "7":
-                displayTray.value +=button.innerText;
+                if (equaltoClicked ==false)displayTray.value += button.innerText
+                else reset(button.innerText); 
+
              break;
              case "8":
-                displayTray.value +=button.innerText;
+                if (equaltoClicked ==false)displayTray.value += button.innerText
+                else reset(button.innerText); 
+
              break;
              case "9":
-                displayTray.value +=button.innerText;
+                if (equaltoClicked ==false)displayTray.value += button.innerText
+                else reset(button.innerText); 
+
              break;
              case "0":
-                displayTray.value +=button.innerText;
+                if (equaltoClicked ==false)displayTray.value += button.innerText
+               else reset(button.innerText); 
+
              break;
              case "00":
-                displayTray.value +=button.innerText;
+                if (equaltoClicked ==false)displayTray.value += button.innerText
+                else reset(button.innerText); 
              break;
+             case "=":
+                display.value = eval(display.value);
+                equaltoClicked=true;
+                break;
+                case "C":
+                    (equaltoClicked==false)?display.value = display.value.toString().slice(0,-1): display.value="";
+                    break;
             default:
             displayTray +=button.innerText;
                 break;
@@ -99,4 +135,12 @@ function multiply(element, dis){
     }
 }
 
+function reset(input){
+
+     let displayValue = displayTray.value;
+    
+     (oper.includes(displayValue[displayValue.length-1]))?displayTray.value += input : displayTray.value = input;
+    equaltoClicked = false;
+    
+    }
 
